880b8196d19c366d07b1812497b9e89af917214b
Gianluca Arbezzano <gianarb92@gmail.com>
2015-10-18 12:41:35 +0200

    Compatibility with PHP7

--- src/Route/FastPsr7RouteInfo.php
+++ src/Route/FastPsr7RouteInfo.php
@@ -1,14 +1,19 @@
 <?php
 namespace Penny\Route;
 
+use InvalidArgumentException;
+
 class FastPsr7RouteInfo implements RouteInfoInterface
 {
     private $name;
     private $callable;
     private $params;
 
-    public static function matched($name, callable $callable, $params = [])
+    public static function matched($name, $callable, $params = [])
     {
+        if (!is_callable($callable)) {
+            throw new InvalidArgumentException('$callable could be only a callable');
+        }
         $obj = new self();
         $obj->name = $name;
         $obj->callable = $callable;
