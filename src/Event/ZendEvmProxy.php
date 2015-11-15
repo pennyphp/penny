db5fa216e94fb54c6e9b33c572cb16e9feffa62a
Gianluca Arbezzano <gianarb92@gmail.com>
2015-11-15 12:09:50 +0100

    Add docblock

--- src/Event/ZendEvmProxy.php
+++ src/Event/ZendEvmProxy.php
@@ -5,19 +5,31 @@ use Zend\EventManager\EventManager;
 
 class ZendEvmProxy implements PennyEvmInterface
 {
+    /**
+     * @var EventManager
+     */
     private $eventManager;
 
+    /**
+     * Proxy Zend\EventManager\EventManager
+     */
     public function __construct()
     {
         $this->eventManager = new EventManager();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function trigger(PennyEventInterface $event)
     {
         $this->eventManager->trigger($event);
         return $this;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function attach($eventName, callable $listener, $priority = 0)
     {
         $this->eventManager->attach($eventName, $listener, $priority);
